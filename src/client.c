eda469954abaff4364d36510d3e7d34bd5dc3f2f
Savannah Greuel <smallidgesavannah@gmail.com>
2019-05-17 08:12:40 -0600

    send_request

--- src/client.c
+++ src/client.c
@@ -101,9 +101,24 @@ int send_request(int fd, char *hostname, char *port, char *path)
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  int request_length = snprintf(request, max_request_size,
+  "GET /%s HTTP/1.1\n"
+  "Host: %s:%s\n"
+  "Connection: close\n"
+  "\n",
+  path, hostname, port);
 
-  return 0;
+  rv = send(fd, request, request_length, 0);
+
+  if (rv < 0)
+  {
+    printf("request err");
+    exit(2);
+  }
+
+  return rv;
 }
+
 
 int main(int argc, char *argv[])
 {  
