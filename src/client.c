862bb621fc2d76013dfffc50ddc47cb5f6c32dad
Savannah Greuel <smallidgesavannah@gmail.com>
2019-05-17 08:09:42 -0600

    parse url

--- src/client.c
+++ src/client.c
@@ -36,22 +36,50 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+//  1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
+  if (strstr(url, "http://"))
+  {
+    hostname = strdup(url + 7);
+  }
+  else if (strstr(url, "https://"))
+  {
+    hostname = strdup(url + 8);
+  }
+  else
+  {
+    hostname = strdup(url);
+  }
 
-    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  // 2. Set the path pointer to 1 character after the spot returned by strchr.
+// 3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
+  if (strchr(hostname, '/'))
+  {
+    path = strchr(hostname, '/') + 1;
+    *(path - 1) = '\0';
+  }
+  else
+  {
+    path = "";
+  }
+  //   4. Use strchr to find the first colon in the URL.
+  //   5. Set the port pointer to 1 character after the spot returned by strchr.
+  //   6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+  
+  if (strchr(hostname, ':'))
+  {
+    port = strchr(hostname, ':') + 1;
+    *(port - 1) = '\0';
+  }
+  else
+  {
+    port = "80";
+  }
+  urlinfo->path = path;
+  urlinfo->port = port;
+  urlinfo->hostname = hostname;
 
   return urlinfo;
+  
 }
 
 /**
